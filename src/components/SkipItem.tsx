74f4f21e807beaae8cbc1f05fc45d2c0b744589d
liya-mary <maryliya007@gmail.com>
2025-06-09 15:10:37 +0200

    Fix: warning badge on top of image

--- src/components/SkipItem.tsx
+++ src/components/SkipItem.tsx
@@ -10,23 +10,31 @@ function SkipItem({item,skipSelectionFunction,selectedSkip}:SkipItemProps) {
   const imageSource=`${item.size}-yarder-skip.jpg`
     return (
       <div >
-        <div onClick={()=>{skipSelectionFunction(item) }}  className={` relative max-w-sm rounded overflow-hidden shadow-lg hover:outline-2 outline-green-300 text-center m-5 bg-stone-100  ${selectedSkip?.id===item.id? 'outline-2 outline-green-600 ':''}`}>
-            <img className="w-full h-58 " src={imageSource} alt="Sunset in the mountains"/>
-            {!item.allowed_on_road &&
-              <WarningBadge top={1} text="Not Allowed On The Road"/>
-            }
+        <div onClick={()=>{skipSelectionFunction(item) }}  className={`max-w-sm rounded overflow-hidden shadow-lg hover:outline-2 outline-green-300 text-center m-5 bg-stone-100  ${selectedSkip?.id===item.id? 'outline-2 outline-green-600 ':''}`}>
+          <div className=" relative">
+            <img className=" w-full h-58 " src={imageSource} alt="Sunset in the mountains"/>
+              {!item.allowed_on_road &&
+                <div className="absolute top-1">
+                  <WarningBadge text="Not Allowed On The Road"/>
+                </div>
 
-            {!item.allows_heavy_waste &&
-              <WarningBadge top={10} text=" Not Suitable For Heavy Waste" />
-            }
+              }
 
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{item.size} Yard Skip</div>
-                <p className="text-gray-700 text-base">
-                    {item.hire_period_days} day hire period
-                </p>
-                <h1 className=" text-xl font-bold  text-green-600">£{item.price_before_vat}</h1>
-            </div>
+              {!item.allows_heavy_waste &&
+                <div className="absolute top-10">
+                  <WarningBadge  text=" Not Suitable For Heavy Waste" />
+                </div>
+              }
+          </div>
+
+
+          <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{item.size} Yard Skip</div>
+              <p className="text-gray-700 text-base">
+                  {item.hire_period_days} day hire period
+              </p>
+              <h1 className=" text-xl font-bold  text-green-600">£{item.price_before_vat}</h1>
+          </div>
         </div>
       </div>
     )
